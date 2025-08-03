492fa81542eb0221f205ae72014de4717f0fb809
Ahmed Hasan <83498124+thisisahmed10@users.noreply.github.com>
2025-08-04 01:37:51 +0300

    Add files via upload

---
+++ login/login.js
@@ -0,0 +1,18 @@
+window.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("login").addEventListener("click", () => {
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    if (email === "" || password === "") {
+      alert("❗ Please enter your email and password.");
+      return;
+    }
+
+    
+    localStorage.setItem("userData", JSON.stringify({ email, password }));
+    window.location.href = "nextPage.html"; 
+  });
+
+  
+});
+
