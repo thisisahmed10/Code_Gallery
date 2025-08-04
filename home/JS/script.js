baba00de2b116e94192d46d37102d4a2e8719dab
Ahmed Hasan <83498124+thisisahmed10@users.noreply.github.com>
2025-08-04 06:48:14 +0300

    Update script.js

--- home/JS/script.js
+++ home/JS/script.js
@@ -21,4 +21,10 @@ function showAbout() {
 function showContact() {
   document.querySelector('.background').style.display = 'none';
   document.getElementById('content').innerHTML = '<iframe src="../about/about.html#contact" width="100%" height="700px" frameborder="0"></iframe>';
+
+}
+
+function showFeed() {
+  document.querySelector('.background').style.display = 'none';
+  document.getElementById('content').innerHTML = '<iframe src="../feed/feed.html" width="100%" height="700px" frameborder="0"></iframe>';
 }
